4b5422dc5f91bc2337b45d0b0186477dfd96ca17
БарановскийА <1423demon@mail.ru>
2019-09-27 17:41:31 +0600

    seed

--- DL/Migrations/Configuration.cs
+++ DL/Migrations/Configuration.cs
@@ -14,27 +14,22 @@ namespace DataLayer.Migrations
 
         protected override void Seed(DataLayer.Entities.Model1 context)
         {
-            DataLayer.Entities.Authors author1 = new Entities.Authors {Id = 1,FirstName = "Alexander", LastName = "Pishkin"};
-            DataLayer.Entities.Authors author2 = new Entities.Authors {Id = 2,FirstName = "Lev", LastName = "Tolstoy"};
-            context.Authors.Add(author1);
-            context.Authors.Add(author2);
-            context.SaveChanges();
-            DataLayer.Entities.Genre genre1 = new Entities.Genre {Id = 1, Name = "Action"};
-            DataLayer.Entities.Genre genre2 = new Entities.Genre {Id = 2, Name = "Detective"};
-            context.Genre.Add(genre1);
-            context.Genre.Add(genre2);
-            context.SaveChanges();
-            DataLayer.Entities.Books book1 = new Entities.Books {Id = 1, AuthorId = 1, Title = "TestBook1", GenreId = 1, Price = 1000, Pages = 150};
-            DataLayer.Entities.Books book2 = new Entities.Books {Id = 2, AuthorId = 2, Title = "TestBook2", GenreId = 2, Price = 3000, Pages = 200};
-            context.Books.Add(book1);
-            context.Books.Add(book2);
-            context.SaveChanges();
-            DataLayer.Entities.Users user1 = new Entities.Users {Id = 1, Email = "testUser1@mail.ru", Name = "TestUser1"};
-            DataLayer.Entities.Users user2 = new Entities.Users {Id = 2, Email = "testUser2@mail.ru", Name = "TestUser2"};
-            context.Users.Add(user1);
-            context.Users.Add(user2);
-            context.SaveChanges();
-
+            DataLayer.Entities.Authors author1 = new Entities.Authors {FirstName = "Alexander", LastName = "Pishkin"};
+            DataLayer.Entities.Authors author2 = new Entities.Authors {FirstName = "Lev", LastName = "Tolstoy"};
+            context.Authors.AddOrUpdate(author1);
+            context.Authors.AddOrUpdate(author2);
+            DataLayer.Entities.Genre genre1 = new Entities.Genre {Name = "Action"};
+            DataLayer.Entities.Genre genre2 = new Entities.Genre {Name = "Detective"};
+            context.Genre.AddOrUpdate(genre1);
+            context.Genre.AddOrUpdate(genre2);
+            DataLayer.Entities.Books book1 = new Entities.Books {AuthorId = 1, Title = "TestBook1", GenreId = 1, Price = 1000, Pages = 150};
+            DataLayer.Entities.Books book2 = new Entities.Books {AuthorId = 2, Title = "TestBook2", GenreId = 2, Price = 3000, Pages = 200};
+            context.Books.AddOrUpdate(book1);
+            context.Books.AddOrUpdate(book2);
+            DataLayer.Entities.Users user1 = new Entities.Users {Email = "testUser1@mail.ru", Name = "TestUser1"};
+            DataLayer.Entities.Users user2 = new Entities.Users {Email = "testUser2@mail.ru", Name = "TestUser2"};
+            context.Users.AddOrUpdate(user1);
+            context.Users.AddOrUpdate(user2);
         }
     }
 }
